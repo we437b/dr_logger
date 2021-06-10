289431bf2074f5de36e779fb0c0d9264fec5b074
Eugene Song <62779745+we437b@users.noreply.github.com>
2021-06-10 10:13:48 +0900

    Add files via upload

--- simulation_ws/runme.sh
+++ simulation_ws/runme.sh
@@ -1,6 +1,6 @@
 colcon build
 # colcon bundle
-source ~/dr_logger-robomaker-sample-application-deepracer/simulation_ws/install/setup.sh
+source ~/dr_logger/simulation_ws/install/setup.sh
 roslaunch deepracer_simulation racetrack_with_racecar.launch world_name:=hard_track gui:=true
-
+roslaunch deepracer_simulation logger.launch
 
